75bfc8d697a2ca82c6fb524e798413b5ca7186fa
olive <01ive@free.fr>
2020-04-03 17:36:33 +0200

    Add parameters to clone

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
-def clone(String url ='none') {
-	git branch: 'feature/jenkins', url: 'https://github.com/01ive/SimpleCppWorkflows.git'
+def clone(String mybranch = "master", String myurl) {
+	git branch: mybranch, url: myurl
 }
 	
 
