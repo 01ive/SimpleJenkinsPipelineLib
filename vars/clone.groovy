398d780ddfa69d042bd21242268cae82773cf17f
olive <01ive@free.fr>
2020-04-03 17:59:34 +0200

    Add setup var

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -3,8 +3,3 @@
 def clone(String mybranch = "master", String myurl) {
 	git branch: mybranch, url: myurl
 }
-	
-
-//def call(String name = 'human') {
-//  echo "Clone , ${name}."
-//}
---
+++ vars/setup.groovy
@@ -0,0 +1,4 @@
+#!/usr/bin/env groovy
+
+String branch = 'feature/jenkins'
+String url = 'https://github.com/01ive/SimpleCppWorkflows.git'
