eac01c2193220634b3380e43822abf32be0c4363
olive <01ive@free.fr>
2020-04-03 18:05:09 +0200

    Add setup getter

--- vars/setup.groovy
+++ vars/setup.groovy
@@ -1,4 +1,9 @@
 #!/usr/bin/env groovy
 
-String branch = 'feature/jenkins'
-String url = 'https://github.com/01ive/SimpleCppWorkflows.git'
+def get_branch() {
+    return 'feature/jenkins'
+}
+
+def get_url() {
+    return 'https://github.com/01ive/SimpleCppWorkflows.git'
+}
